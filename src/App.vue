273b849d64c20551d9441142d90f2d9fabba300f
Yuji <yuji08752@gmail.com>
2023-12-03 20:49:40 +0900

    Vue ファイルを作りました

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/post-memo">Post memo</router-link> |
+    <router-link to="/map-view">Map view</router-link> |
+    <router-link to="/map-form">Map form</router-link>
   </nav>
   <router-view />
 </template>
---
+++ src/views/MapForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Map Formだよ</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
