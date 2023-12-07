2d50969c699db4791ce20355aa5ad3518f887b48
iwahashi0910 <i.yuya0910@gmail.com>
2023-12-07 22:23:13 +0900

    MapFormの途中まで3

--- src/views/MapForm.vue
+++ src/views/MapForm.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="nyuryokuran">
-    <h2>入力フォーム</h2>
+    <h2 class="nyuryoku">入力フォーム</h2>
     場所：
-    <input class="MapPlace" type="text" v-model="mapplace" />
+    <input class="textbox" type="text" v-model="mapplace" />
     滞在時間：
-    <select class="StayingTime" v-model="stayingtime">
+    <select class="textbox" v-model="stayingtime">
       <option>10分</option>
       <option>20分</option>
       <option>30分</option>
@@ -15,7 +15,7 @@
       <option>3時間</option>
     </select>
     移動方法：
-    <select class="MovingMethod" v-model="movingmethod">
+    <select class="textbox" v-model="movingmethod">
       <option>徒歩</option>
       <option>自転車</option>
       <option>電車</option>
@@ -24,10 +24,10 @@
       <option>新幹線</option>
       <option>飛行機</option>
     </select>
-    <div>
-      <button v-on:click="tuika">追加</button>
-    </div>
   </section>
+  <div>
+    <button class="tuika-button" v-on:click="tuika">追加</button>
+  </div>
   <section>
     <h2>追加記録</h2>
     <div v-if="logs.length > 0">
@@ -62,18 +62,32 @@ export default {
 
 <style scoped>
 .nyuryokuran {
-  display: flex;
-  flex-direction: row;
-  position: relative;
   background-color: beige;
-  margin: 100px;
-  border-radius: 20px;
+  justify-content: space-around;
+  align-items: center;
+  width: 1000px;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: 20px;
+  border-radius: 50px;
   border: solid;
 }
-.MapPlace {
-  padding: 20px;
-  width: 10%;
-  padding-top: 4rem rem;
-  padding-left: 5rem;
+.tuika-button {
+  margin: 20px /*0 auto*/;
+  width: 15rem;
+  height: 4rem;
+  border-radius: 10px 10px 10px 10px;
+  background-color: beige;
+  font-size: 1.8rem;
+}
+.textbox {
+  margin: 0 auto;
+  width: 200px;
+  height: 50px;
+  border-radius: 3px 3px 3px 3px;
+  background-color: white;
+  margin: 1rem;
 }
 </style>
