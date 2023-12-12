7e66946ca7a3f93c04886297e40c1c92056c955b
WatanabeRaibu <raibu0512@outlook.jp>
2023-12-12 22:06:51 +0900

    メモ作成

--- src/views/PostMemo.vue
+++ src/views/PostMemo.vue
@@ -1,9 +1,47 @@
 <template>
   <div>Post memoだよ</div>
+  <div class="memo-list">
+    <ul class="memo-list__container">
+      <li v-for="(memo, index) in memos" v-bind:key="index" class="memo">
+        <div class="memo__checkbox">
+          <input type="checkbox" v-model="memo.isDone" />
+        </div>
+        <div v-if="memo.isDone" class="memo__text memo__text--done">
+          {{ index }}:{{ memo.text }}
+        </div>
+        <div v-else class="memo__text">{{ index }}:{{ memo.text }}</div>
+      </li>
+    </ul>
+    <div class="add-memo-field">
+      <input class="add-memo-field__input" type="text" v-model="inputMemo" />
+      <button class="add-memo-field__button" v-on:click="postMome">追加</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { collection, addDoc } from "firebase/firestore"
+// firebase.js で db として export したものを import
+import { db } from "../firebase.js"
+export default {
+  data() {
+    return {
+      inputMemo: "",
+      memos: [],
+    }
+  },
+  methods: {
+    postMome() {
+      const memo = { text: this.inputMemo, isDone: false }
+      addDoc(collection(db, "memos"), memo).then((ref) => {
+        this.memos.push({
+          id: ref.id,
+          ...memo,
+        })
+      })
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
